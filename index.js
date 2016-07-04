34ce7565ca6459e681a8ac3a38bc8668d9de2703
SylvainLap <sylvain.lapendry@gmail.com>
2016-07-04 15:55:44 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const ParseServer = require('parse-server').ParseServer;
+
+const databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+
+const api = new ParseServer({
+  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+  appId: process.env.APP_ID || 'myAppId',
+  masterKey: process.env.MASTER_KEY || 'myMasterKey',
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',
+});
+
+const app = express();
+
+const mountPath = process.env.PARSE_MOUNT || '/parse';
+app.use(mountPath, api);
+
+app.get('/', function(req, res) {
+  res.status(200).send('I dream of being a website!');
+});
+
+const port = process.env.PORT || 1337;
+const httpServer = require('http').createServer(app);
+httpServer.listen(port, function() {
+  console.log('parse backend running on port ' + port + '.');
+});
